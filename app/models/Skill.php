f6e999f0c15d0d469efb49f983bfc12aa8d3448c
Chris <justwhenithought@gmail.com>
2014-06-08 02:34:57 +0800

    'skill'

---
+++ app/models/Skill.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableInterface;
+
+class Skill extends Eloquent implements UserInterface, RemindableInterface {
+
+
+}

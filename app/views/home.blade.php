a6ebed5f3381f4dbacad64a16fb26a6aadb1b6ad
berman82312 <berman82312@gmail.com>
2014-06-08 13:53:09 +0800

    fix home.php conflict

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -24,7 +24,6 @@
 							<div style="text-align: center; font-size:22px;font-weight: bold;" class="field">
 								<span>Dashboard</span>
 								<div class="field">
-<<<<<<< HEAD
 								@if(Auth::check())
 									<div class="field">
 									<div class="profile-btn post" style="background-color:#00cbe9;color:white;border:none;box-shadow: 1px 1px 1px #aaa;">發案</div></div>
@@ -41,20 +40,6 @@
 								<div class="field">
 									<a class="fluid ui purple button">個人資料</a>
 								</div>
-=======
-								<div class="profile-btn post" style="background-color:#00cbe9;color:white;border:none;box-shadow: 1px 1px 1px #aaa;">發案</div></div>
-							@endif
-								<div class="profile-btn" style="background-color:#00cbe9;color:white;border:none;box-shadow: 1px 1px 1px #aaa;">我接的案</div>
-							</div>
-							<div class="field">
-								<div class="profile-btn" style="background-color:#fd8a33;color:white;border:none;box-shadow: 1px 1px 1px #aaa;">我發的案</div>
-							</div>
-							<div class="field">
-								<div class="profile-btn" style="background-color:#8bc53f;color:white;border:none;box-shadow: 1px 1px 1px #aaa;">歷史記錄</div>
-							</div>
-							<div class="field">
-								<a href="profile"><div class="profile-btn" style="background-color:#ff82b5;color:white;border:none;box-shadow: 1px 1px 1px #aaa;">個人資料</div></a>
->>>>>>> 2a484cfefc1c805f48da6ff228afae10536ba555
 							</div>
 						</div>
 					</div>
@@ -75,169 +60,7 @@
 				</div>
 			</div>
 		</div>
-<<<<<<< HEAD
-		<div class = "row">
-			<div class="three column stackable ui grid">
-				<?php //foreach ($works as $work) {?>
-				<div class="column">
-					<div class="ui segment">
-						<div class="ui dimmer">
-							<div class="content">
-								<div class="task-desc">
-									<p>敘述：<?php //echo $work->description; ?></p>
-								</div>
-								<div class="task-choose">
-									<a class="ui green button" style="width:200px;text-align:center;">接任務</a>
-								</div>
-							</div>
-						</div>
-						<div class="task-label">HOT</div>
-						<div class="field">
-							<img src="img/<?php //echo $work->img?>" class="head-profile">
-						</div>
-						<div class="field">
-							<div class="task-title">任務：<?php //echo $work->name; ?></div>
-						</div>
-						<div class="field">
-							<div class="task-creator">發起人:<?php //echo $work->creator;?></div>
-						</div>
-						<div class="field">
-							<div class="task-date"><?php //echo $work->dueTime;?></div>
-						</div>
-					</div>
-				</div>
-				<?php //}?>
-				<div class="column">
-					<div class="ui segment">
-						<div class="ui dimmer">
-							<div class="content">
-								<div class="task-desc">
-									<p >敘述：</p>
-									<p>幫助警察建立工會blablabla^____^</p>
-									<p>協助警察建立良好工作環境</p>
-								</div>
-								<div class="task-choose">
-									<a class="ui green button" style="width:200px;text-align:center;">接任務</a>
-								</div>
-							</div>
-						</div>
-						<div class="task-label">HOT</div>
-						<div class="field">
-							<img class="head-profile" src="img/police.jpg"/>
-						</div>
-						<div class="field">
-							<div class="task-title">事情：幫忙警察組工會</div>
-						</div>
-						<div class="field">
-							<div class="task-title">發起人：洨魯</div>
-						</div>
-						
-						<div class="field">
-							<div class="task-date">5/21</div>
-						</div>
-					</div>
-				</div>
-				<div class="column">
-					<div class="ui segment">
-						<div class="ui dimmer">
-							<div class="content" >
-								<div class="task-desc">
-									<p >敘述：</p>
-									<p>徵求正妹伴遊政大</p>
-									<p>哥有30cm^__^</p>
-								</div>
-								<div class="task-choose">
-									<a class="ui green button" style="width:200px;text-align:center;">接任務</a>
-								</div>
-							</div>
-						</div>
-						<div class="field">
-							<img class="head-profile" src="img/ccty.jpg"/>
-						</div>
-						<div class="field">
-							<div class="task-title">事情：陪我去政大</div>
-						</div>
-						<div class="field">
-							<div class="task-title">發起人：洨帥</div>
-						</div>
-						
-						<div class="field">
-							<div class="task-date" >5/21</div>
-						</div>
-					</div>
-				</div>
-				<div class="column">
-					<div class="ui segment">
-						<div class="field">
-							<img class="head-profile" src="img/police.jpg"/>
-						</div>
-						<div class="field">
-							<div class="task-title">事情：幫忙警察組工會</div>
-						</div>
-						<div class="field">
-							<div class="task-title">發起人：洨魯</div>
-						</div>
-						
-						<div class="field">
-							<div class="task-date">5/21</div>
-						</div>
-					</div>
-				</div>
-				<div class="column">
-					<div class="ui segment">
-						<div class="field">
-							<img class="head-profile" src="img/ccty.jpg"/>
-						</div>
-						<div class="field">
-							<div class="task-title">事情：陪我去政大</div>
-						</div>
-						<div class="field">
-							<div class="task-title">發起人：洨帥</div>
-						</div>
-						<div class="field">
-							<div class="task-date">5/23</div>
-						</div>
-					</div>
-				</div>
-				<div class="column">
-					<div class="ui segment">
-						<div class="field">
-							<img class="head-profile" src="img/penis.jpg"/>
-						</div>
-						<div class="field">
-							<div class="task-title">事情：幫我領獎</div>
-						</div>
-						<div class="field">
-							<div class="task-title">發起人：洨小魯</div>
-						</div>
-						
-						<div class="field">
-							<div class="task-date">5/21</div>
-						</div>
-					</div>
-				</div>
-				<div class="column">
-					<div class="ui segment">
-						<div class="field">
-							<img class="head-profile" src="img/police.jpg"/>
-						</div>
-						<div class="field">
-							<div class="task-title">事情：幫忙警察組工會</div>
-						</div>
-						<div class="field">
-							<div class="task-title">發起人：洨魯</div>
-						</div>
-						
-						<div class="field">
-							<div class="task-date">5/21</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-=======
 		@yield('right-container')
->>>>>>> 2a484cfefc1c805f48da6ff228afae10536ba555
 	</div>
 	</article>
 
